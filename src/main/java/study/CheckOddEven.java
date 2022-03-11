787bb026985f7576bf21ebdfc78034967fbc774c
pranav wagh <waghpranav222@gmail.com>
2022-03-11 16:01:59 +0530

    first commit

--- src/main/java/study/CheckOddEven.java
+++ src/main/java/study/CheckOddEven.java
@@ -1,14 +1,20 @@
 package study;
 
 public class CheckOddEven {
+	/*
 	public static void main(String[] args) {
 		
 		System.out.println("!!!....Result :"+checkOddEven(22)+"....!!!");
 		
-	}
+	}*/
 	public static boolean checkOddEven(int num)
 	{
 		return num%2 == 0;
 	}
-
+	
+	public static boolean checkThreeDivisibility(int num)
+	{
+		boolean res = (num%3==0);
+		return res;
+	}
 }
